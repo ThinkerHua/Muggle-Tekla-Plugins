f26ff8e942fa72fb80e5c92737d0f6103f736d4c
ThinkerHua <thinkerhua@hotmail.com>
2025-08-18 21:56:35 +0800

    添加TSEP；主程序添加logo；

--- MainWindow/Converters/PluginNameToIconValueConverter.cs
+++ MainWindow/Converters/PluginNameToIconValueConverter.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
 
 namespace Muggle.TeklaPlugins.MainWindow.Converters {
     public class PluginNameToIconValueConverter : IValueConverter {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-            var uriStr = string.Format("/Resources/et_element_{0}.bmp", (string)value);
-            return new BitmapImage(new Uri(uriStr, UriKind.Relative));
+            var currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            //  用于Tekla Structures目录
+            var uriStr = new DirectoryInfo(
+                string.Format(
+                    @"{0}..\..\..\..\Bitmaps\et_element_{1}.bmp",
+                    currentDirectory,
+                    value as string))
+                .FullName;
+
+            //  用于VS工程目录
+            if (!File.Exists(uriStr)) {
+                uriStr = new DirectoryInfo(
+                    string.Format(
+                        @"{0}..\bitmaps\et_element_{1}.bmp",
+                        currentDirectory,
+                        value as string))
+                    .FullName;
+            }
+
+            return new BitmapImage(new Uri(uriStr, UriKind.Absolute));
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
