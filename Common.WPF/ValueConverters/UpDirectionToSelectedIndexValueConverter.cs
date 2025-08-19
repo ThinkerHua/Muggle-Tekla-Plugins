fa850fcbc54392d224956bab66a2b09822e0ea4e
ThinkerHua <thinkerhua@hotmail.com>
2025-08-19 12:24:14 +0800

    更新 Common 文档；

--- Common.WPF/ValueConverters/UpDirectionToSelectedIndexValueConverter.cs
+++ Common.WPF/ValueConverters/UpDirectionToSelectedIndexValueConverter.cs
@@ -4,6 +4,9 @@ using System.Windows;
 using System.Windows.Data;
 
 namespace Muggle.TeklaPlugins.Common.WPF.ValueConverters {
+    /// <summary>
+    /// 用于将组件通用选项卡中的向上方向枚举值转换为ComboBox的选中索引。
+    /// </summary>
     public class UpDirectionToSelectedIndexValueConverter : IValueConverter {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
             if (value is not int index) return DependencyProperty.UnsetValue;
