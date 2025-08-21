2c7c6d6fa7f6beb7fdec7654228dd8b783c73730
ThinkerHua <thinkerhua@hotmail.com>
2025-08-21 17:57:24 +0800

    Common.WPF, MainWindow: EnumToBoolValueConverter 从 MainWindow 移至 Common.WPF；

---
+++ Common.WPF/ValueConverters/EnumToBoolValueConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Muggle.TeklaPlugins.Common.WPF.ValueConverters {
+    /// <summary>
+    /// Converts an enumeration value to a boolean and vice versa for use in data binding scenarios.
+    /// </summary>
+    /// <remarks>This value converter is typically used to bind an enumeration value to a boolean property in
+    /// the UI.  The <see cref="Convert"/> method checks if the provided value matches the parameter and returns a
+    /// boolean result.  The <see cref="ConvertBack"/> method converts a boolean value back to the corresponding
+    /// enumeration value.</remarks>
+    public class EnumToBoolValueConverter : IValueConverter {
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return value?.Equals(parameter) ?? DependencyProperty.UnsetValue;
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            return value?.Equals(true) == true ? parameter : Binding.DoNothing;
+        }
+    }
+}
